c0161f14a42f09f2d8c7e83a3c9a5268c5c0e584
jayeshpatil665 <jayeshpatil665@gmail.com>
2023-09-10 15:13:34 +0530

    FirstCommit ProjectInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'linkshub';
+
+  homePageTitle : string='Links-Hub : your ultimate links dock !';
+  projectVersion : string='v 0.0.1';
+
+  constructor(){
+    console.log('Starting...');
+  }
+
+  ngOnInit(): void {
+    console.log('Project init success ! - ',this.projectVersion);
+  }
+
 }
